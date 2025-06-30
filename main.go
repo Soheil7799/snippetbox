6de8e6d8ea23ebf1386afbf7d3d6854c52b0e0a5
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-06-30 13:59:28 +0200

    Update to have web server and listen and response with a message

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name != "" {
+		w.Write([]byte("Hello " + strings.ToUpper(name) + "\nWelcome to my snippetBox Experiment"))
+	} else {
+		w.Write([]byte("Hello from SnippetBox Home"))
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Starting server on : 4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
