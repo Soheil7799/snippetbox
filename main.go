5771a22ebaab940a45e6a4f84f4368df9c3c76bf
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-06-30 16:08:14 +0200

    Add query strings to snippet/view

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -20,10 +22,28 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Here We should show some specific snippet i think !..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		w.Write([]byte("No id was found\nDefault snippet view"))
+		return
+	} else {
+		// w.Write([]byte(fmt.Sprintf("Here is the snippet with the id: %d", id)))
+		fmt.Fprintf(w, "Here is the snippet with the id: %d", id)
+	}
+
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		// w.WriteHeader(405)
+		// w.Write([]byte("Need POST method to work !"))
+		http.Error(w, "Need POST metod to work !", http.StatusMethodNotAllowed)
+		return
+	}
+	// w.Header().Set("Content-Type", "application/json")
+	// w.Write([]byte(`{"name":"soheil"}`))
 	w.Write([]byte("Here I think user could create some snippets ?!..."))
 }
 
