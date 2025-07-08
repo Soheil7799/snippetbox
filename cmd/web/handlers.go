9d5b2f12f97764de0460bf26ccd514490eed4c6c
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-07-08 15:57:17 +0200

    Update insert and get handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/Soheil7799/snippetbox/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -38,22 +41,38 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		w.Write([]byte("No id was found\nDefault snippet view"))
 		return
-	} else {
-		// w.Write([]byte(fmt.Sprintf("Here is the snippet with the id: %d", id)))
-		fmt.Fprintf(w, "Here is the snippet with the id: %d", id)
 	}
 
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	//fmt.Println("before method check")
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		// w.WriteHeader(405)
-		// w.Write([]byte("Need POST method to work !"))
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	// w.Header().Set("Content-Type", "application/json")
-	// w.Write([]byte(`{"name":"soheil"}`))
-	w.Write([]byte("Here I think user could create some snippets ?!..."))
+	//fmt.Println("handler called with post (after method check)")
+	title := "0 snail"
+	content := "0 snail\nClimb mount fuji,\nBut slowly slowly!\n\n- Kobayashi Issa"
+	expires := 7
+	//fmt.Println("before calling insert")
+	id, err := app.snippets.Insert(title, content, expires)
+	//fmt.Println("after calling insert")
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	//fmt.Sprintf("after error checking insert with id: %d", id)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Soheil7799/snippetbox/internal/models"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logErr  *log.Logger
-	logInfo *log.Logger
+	logErr   *log.Logger
+	logInfo  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func openDB(dsn string) (*sql.DB, error) {
@@ -30,11 +32,6 @@ func main() {
 	logInfo := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	logErr := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	app := &application{
-		logErr:  logErr,
-		logInfo: logInfo,
-	}
-
 	addr := flag.String("addr", ":4000", "HTTP network listening address")
 	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
@@ -44,6 +41,11 @@ func main() {
 	}
 	defer db.Close()
 
+	app := &application{
+		logErr:   logErr,
+		logInfo:  logInfo,
+		snippets: &models.SnippetModel{DB: db},
+	}
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: logErr,
