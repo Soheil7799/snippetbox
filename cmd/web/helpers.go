4d2d7dbc6f0a0a0c919bc6cceed608625d2d9adc
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-07-19 22:22:48 +0200

    Add CurrentYear field to templateData for dynamic year rendering

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -35,4 +36,12 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 	w.WriteHeader(status)
 
 	_, _ = buffer.WriteTo(w)
+}
+
+func (app *application) newTemplateData(r *http.Request) templateData {
+
+	return templateData{
+		CurrentYear: time.Now().Year(),
+	}
+
 }
